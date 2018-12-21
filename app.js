3ccb89f4128847f104155470d3dce6eb299d7510
jesse.adams <jesse.adams@servicenow.com>
2018-12-21 16:01:38 -0500

    Now with comments

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+// ***************** createNewComponent.js *******************
+// Create all of the files needed for a new react/redux component
+// in a new directory.
+//
+// Author: Jesse Adams
+// Date: 12/21/2018
+
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('.config', 'utf-8'))// write some files
-
 const generateComponent = require(config.componentTemplate);
 const generateActions = require(config.actionTemplate);
 const generateReducer = require(config.reducerTemplate);
-// refer to https://ourcodeworld.com/articles/read/297/how-to-create-a-file-using-the-filesystem-fs-module-in-node-js
 
-// call the generators and then write the files to a directory.
-
-// print command line args
+// get command line args
 let componentName = process.argv[2];
 let shouldBuildActions = process.argv[3] || false;
 let shouldBuildReducers = process.argv[4] || false;
@@ -18,14 +21,18 @@ if (!componentName) {
     return;
 }
 
+// create the new directory if needed and write files
 try {
     try{
+        // assume directory already exists
         process.chdir(`${config.componentRoot}/${componentName}`);
     }catch(e){
+        // oops.. guess it doesn't
         console.warn("Component directory not found in "+config.componentRoot+" creating new directory...");
         fs.mkdirSync(`${config.componentRoot}/${componentName}`)
         process.chdir(`${config.componentRoot}/${componentName}`);
     }
+    // now that we have a directory and are in it, let's write the files
     console.log("Writing files from templates...");
     _writeToFile(generateComponent(componentName),componentName);
     if(shouldBuildActions)
@@ -33,6 +40,7 @@ try {
     if(shouldBuildReducers)
         _writeToFile(generateReducer(componentName),`${componentName}Reducer`);
 } catch (e) {
+    // how did I get here?
     console.error(e);
 }
 
