541a8453c52e91838f003652f6ab389af3eb3df3
Jesse Adams <nobodyknowswhereheis@yahoo.com>
2018-12-23 09:53:34 -0500

    remove commas from actions

--- generateActions.js
+++ generateActions.js
@@ -3,11 +3,11 @@ module.exports = function generateActions(compName,default_actions){
   var actions = default_actions.map((element,index)=>{
     var name = compName.toUpperCase()+element;
      return `
-     export const ${name} = '${name}';
-     `;
+     export const ${name} = '${name}';`;
   });
+  var clean = actions.toString().replace(',','\n');
     const template = `
-    ${actions}
+    ${clean}
     `;
     
     return template;
